08a57b4cb03439e1dcb42a342c4fe3ee5ac85ba3
yerdi <yerdi.isman@gmail.com>
2023-01-22 22:51:19 +0600

    first commit

---
+++ Accessibility-practice/Accessibility_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accessibility_practiceApp.swift
+//  Accessibility-practice
+//
+//  Created by Yerdaulet Ismanaliyev on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Accessibility_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
